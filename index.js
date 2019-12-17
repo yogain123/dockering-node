758288e07ea70546b851a79200b73e5ae34f9a7c
yogain123 <yogendrasaxena56@gmail.com>
2019-12-17 15:39:35 +0530

    few fix

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ var app = express();
 var cors = require('cors');
 app.use(cors());
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 1234;
+const port = process.env.PORT || 4444;
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
 var routing = require("./routing");
 
 app.set('view engine', 'pug');
+
+//app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+console.log("loggin");
 
 app.use(bodyParser.json());
 
--- routing.js
+++ routing.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 router.get("/", function (req, res, next) {
   console.log("Basic Route Hit");
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/public/index.html`);
+})
+
+router.get("/hello", function (req, res, next) {
+  console.log("Basic Route Hit");
+  res.json({
+    name:"yogendra",
+    age: 25
+  })
 })
 
 module.exports = router;
