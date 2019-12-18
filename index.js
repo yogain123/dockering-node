a883d7414cd8d3f537bd4385ccd50b2c1289aac3
yogain123 <yogendrasaxena56@gmail.com>
2019-12-18 16:47:26 +0530

    few fix

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
+const express = require("express");
 var app = express();
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 4444;
-const connectDB = require('./config/db');
-const path = require('path');
+const connectDB = require("./config/db");
+const path = require("path");
+require("dotenv").config();
 
-connectDB();
+//connectDB();
 
 var routing = require("./routing");
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 //app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
-console.log("loggin");
-
 app.use(bodyParser.json());
 
-app.use('/', routing);
+app.use("/", routing);
 app.listen(port);
--- routing.js
+++ routing.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.get("/", function(req, res, next) {
   console.log("Basic Route Hit");
   res.sendFile(`${__dirname}/public/index.html`);
-})
+});
 
-router.get("/hello", function (req, res, next) {
+router.get("/hello", function(req, res, next) {
+  console.log("Basic Route Hit");
   res.json({
-    name:"yogendra",
-    age: 25
-  })
-})
+    name: "yogendra",
+    age: 25,
+    node_env: process.env.NODE_ENV,
+    x_access_token: process.env.x_access_token
+  });
+});
 
 module.exports = router;
